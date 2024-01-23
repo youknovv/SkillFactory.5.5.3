17b27a41207580060da3a4dfefac7b72777a2975
Felmin Encode <youknovv@mail.ru>
2024-01-23 17:38:05 +0700

    Добавьте файлы проекта.

---
+++ SkillFactory.5.5.3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+
+namespace SkillFactory._5._5._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Напишите что-нибудь");
+            var str = Console.ReadLine();
+
+            Console.WriteLine("Укажите глубину эха");
+            var deep = int.Parse(Console.ReadLine());
+
+            Echo(str, deep);
+
+            Console.ReadKey();
+        }
+
+        static void Echo(string saidword, int deep)
+        {
+            var modif = saidword;
+
+            if (modif.Length > 2)
+            {
+                modif = modif.Remove(0, 2);
+            }
+            Console.WriteLine("..." + modif);
+
+            if (deep > 1)
+            {
+                Echo(modif, deep - 1);
+            }
+        }
+    }
+}
